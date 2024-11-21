2b987961e9e4fdf2c318167b9cc2ed9e99b65ef3
Pietro Ballarin <pieballa@gmail.com>
2024-11-21 11:34:32 +0100

    dev: function comments

--- include/lidardriver.hpp
+++ include/lidardriver.hpp
@@ -5,8 +5,13 @@
 #include <ostream>
 
 namespace lidar {
+    // The maximum buffer dimension of the lidar driver
     constexpr int BUFFER_DIM = 10;
 
+    /**
+     * LidarDriver class used to manage the data of a lidar.
+     * It uses an internal buffer managed by sliding window method.
+     */
     class LidarDriver {
         private:
             int start;
@@ -15,13 +20,35 @@ namespace lidar {
             std::vector<std::vector<double>> buffer;
 
         public:
+            /**
+             * Constructs the LidarDriver declaring the reading resolution
+             * @param resolution The angle interval of the readings
+             */
             LidarDriver(double resolution);
+            /**
+             * Retrieves the oldest scan and removes it from the buffer
+             */
             std::vector<double> get_scan() const;
-            void new_scan (std::vector<double> scan);
+            /**
+             * Adds a new scan into the buffer, if full overwrites the oldest scan
+             * @param scan The new scan to add to the buffer
+             */
+            void new_scan(std::vector<double> scan);
+            /**
+             * Retrieves the distance of an object at a given angle 
+             * of the latest read.
+             * @param angle The angle of the reading
+             */
             double get_distance(double angle) const;
+            /**
+             * Removes all the readings from the buffer.
+             */
             void clear_buffer();
     };
 
+    /**
+     * Streams the content of the buffer
+     */
     std::ostream &operator<<(std::ostream &os, LidarDriver &ld);
 }
 
--- src/lidardriver.cpp
+++ src/lidardriver.cpp
@@ -1,24 +1,55 @@
 #include "include/lidardriver.hpp"
 
 namespace lidar {
+
+    /*********************************************************
+     * LidarDriver class member functions
+     *********************************************************/
+
+    /**
+     * Constructs the LidarDriver declaring the reading resolution
+     * @param resolution The angle interval of the readings
+     */
     LidarDriver::LidarDriver(double resolution) {
 
     }
+    /**
+     * Retrieves the oldest scan and removes it from the buffer
+     */
     std::vector<double> LidarDriver::get_scan() const {
 
     }
+    /**
+     * Adds a new scan into the buffer, if full overwrites the oldest scan
+     * @param scan The new scan to add to the buffer
+     */
     void LidarDriver::new_scan(std::vector<double> scan) {
 
     }
+    /**
+     * Retrieves the distance of an object at a given angle
+     * of the latest read.
+     * @param angle The angle of the reading
+     */
     double LidarDriver::get_distance(double angle) const {
 
     }
+
+    /**
+     * Removes all the readings from the buffer.
+     */
     void LidarDriver::clear_buffer() {
 
     }
 
+    /******************************************************************
+     * LidarDriver class helper functions
+     ******************************************************************/
 
-    std::ostream& operator<< (std::ostream& os, LidarDriver& ld) {
+    /**
+     * Streams the content of the buffer
+     */
+    std::ostream& operator<<(std::ostream& os, LidarDriver& ld) {
 
     }
 }
