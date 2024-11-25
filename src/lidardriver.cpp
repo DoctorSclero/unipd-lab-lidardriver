21b6f8c856508167e3913683d251c7d3c9b8ca2f
Pietro Ballarin <pieballa@gmail.com>
2024-11-25 15:39:03 +0100

    feat: operator<< template

--- src/lidardriver.cpp
+++ src/lidardriver.cpp
@@ -31,35 +31,46 @@ namespace lidar {
     /**
      * Adds a new scan into the buffer, if full overwrites the oldest scan
      * @param scan The new scan to add to the buffer
+     * @author Pietro Ballarin
      */
     void LidarDriver::new_scan(std::vector<double> scan) {
-
+        scan.resize(180/resolution + 1, 0.0);
+        if (this->stop == this->start) this->start = (this->start + 1) % BUFFER_DIM;
+        this->buffer[this->stop] = scan;
+        this->stop = (this->stop + 1) % BUFFER_DIM;
     }
     /**
      * Retrieves the distance of an object at a given angle
      * of the latest read.
      * @param angle The angle of the reading
-     */
+     * @author Pietro Ballarin
+     */ 
     double LidarDriver::get_distance(double angle) const {
-
+        return this->buffer[this->start].at(angle * this->resolution);
     }
 
     /**
      * Removes all the readings from the buffer.
+     * @author Elisa Chiarel
      */
     void LidarDriver::clear_buffer() {
-
+        this->start = 0;
+        this->stop = 0;
     }
 
+    // TODO: elisa: get_latest
+    
     /******************************************************************
      * LidarDriver class helper functions
      ******************************************************************/
 
     /**
      * Streams the content of the buffer
+     * @param os The output stream
+     * @param ld The LidarDriver instance
+     * @author Elisa Chiarel
      */
     std::ostream& operator<<(std::ostream& os, LidarDriver& ld) {
-
+        return os;
     }
 }
-
